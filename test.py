8a288cde582254cbddd110981bf63f12ff841e7c
page404 <page404@users.noreply.github.com>
2020-07-14 15:31:06 +0800

    add file

---
+++ test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding:utf-8
+# ***************************************************
+# Author: pagezhang
+# CreateDate: 2020/7/14 
+# File: test.py 2020/7/14 14:52 pagezhang
+# Brief: python file
+# ***************************************************
+import os
+import sys
+
+
+def test():
+    print('hello world.')
+
+
+if __name__ == '__main__':
+    test()
